5e95d3cf31073dcbeacfe10da9c063ed08112946
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-04-17 20:28:44 +0100

    Format code

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ next.addEventListener('click', nextSlide)
 prev.addEventListener('click', prevSlide)
 
 function nextSlide() {
+    const width = firstChild.clientWidth * 4;
+    const difference = width - slider.scrollLeft;
+    if(difference < 2) {
+        slider.append(slider.children[0])
+    }
     slider.scrollLeft+= firstChild.clientWidth
 };
 
 function prevSlide() {
+    if(slider.scrollLeft == 0) {
+        slider.prepend(slider.children[slider.children.length - 1])
+    }
     slider.scrollLeft-= firstChild.clientWidth
 };
